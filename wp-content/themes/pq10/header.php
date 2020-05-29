20dd4ffd926f6a0625ef849a09688b220eadff56
Garrett Cullen <Garth619@users.noreply.github.com>
2020-05-29 10:48:05 -0700

    push

--- wp-content/themes/pq10/header.php
+++ wp-content/themes/pq10/header.php
@@ -55,8 +55,70 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<header>
+
+<div id='header_left'>
+
+	<a class='logo' href='<?php bloginfo('url');?>'>
 	
-<?php // wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'main_menu' ) ); ?>
+			<?php echo file_get_contents( get_template_directory() . '/images/hilley/logo.svg' ) ?>
+	
+	</a><!-- logo -->
+
+</div><!-- header_left -->
+
+<div id='header_middle'>
+
+	<div class='menu_wrapper'>
+	
+		<div class='menu_bars'>
+		
+			<span></span>
+			<span></span>
+			<span></span>
+		
+		</div><!-- menu_bars -->
+
+		<span class="menu_verbiage">Menu</span><!-- menu_verbiage -->
+
+		<div class='menu_close'>
+		
+			<div class='menu_close_x'>
+			
+				<span></span>
+			
+			</div><!-- menu_close_x -->
+
+			<span class="menu_verbiage">Menu</span><!-- menu_verbiage -->
+		
+		</div><!-- menu_close -->
+	
+	</div><!-- menu_wrapper -->
+
+</div><!-- header_middle -->
+
+<div id='header_right'>
+
+<div id='header_right_inner'>
+	
+	<div class='consult_cta'>
+	
+		<span class='consult_verbiage'>Free Consultation</span><!-- consult_verbiage -->
+
+		<span class='consult_verbiage'>Hablamos Español</span><!-- consult_verbiage -->
+	
+	</div><!-- consult_cta -->
+
+	<a id='header_phone' href='tel:4042336200'>(404) 233-6200</a><!-- header_phone -->
+
+	</div><!-- header_right_inner -->
+
+	<nav id="top_nav"><?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'main_menu' ) ); ?></nav>
+
+</div><!-- header_right -->
+
+</header>
+	
 
 
-<span>sup</span>
