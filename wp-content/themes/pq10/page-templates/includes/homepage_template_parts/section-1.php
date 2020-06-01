bffe07f4dd445ffb26f897a1b48ca2c1e7d3afbf
Garrett Cullen <Garth619@users.noreply.github.com>
2020-06-01 11:02:33 -0700

    push

--- wp-content/themes/pq10/page-templates/includes/homepage_template_parts/section-1.php
+++ wp-content/themes/pq10/page-templates/includes/homepage_template_parts/section-1.php
@@ -1,5 +1,19 @@
 <section id='section_one'>
 
+  <div id='sec_one_inner'>
   
+    <div id='sec_one_right'>
+    
+      <span id='sec_one_subtitle'>Georgia's Award Winning Team</span><!-- sec_one_subtitle -->
+    
+    </div><!-- sec_one_right -->
+  
+  </div><!-- sec_one_inner -->
+
+  <picture>
+
+    <img id='' src='<?php bloginfo('template_directory');?>/images/hero-mob.jpg' />
+
+  </picture>
 
 </section><!-- section_one -->
