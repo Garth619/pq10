f6e78cf9fd091454e49b290d5886ce64e97d3510
Garrett Cullen <Garth619@users.noreply.github.com>
2020-06-04 15:29:02 -0700

    push

--- wp-content/themes/pq10/footer.php
+++ wp-content/themes/pq10/footer.php
@@ -1,4 +1,34 @@
+<footer id='consultation'>
 
+	<div id='footer_form_wrapper'>
+
+		<div id='footer_form_left'>
+		
+			<div id='footer_form_inner'>
+		
+				<span id='footer_form_title'>Request a free consultation</span><!-- footer_form_title -->
+
+				<div id='footer_desc'>
+			
+					<p>There is never a fee to talk to a lawyer at Hilley & Frieder about your case. Call <a href="tel:4042336200">(404) 233-6200</a> or complete our contact form for a free and confidential consultation.</p>
+			
+				</div><!-- footer_desc -->
+
+				<span id='schedule'>Schedule Your Free Consultation Today</span><!-- schedule -->
+		
+			</div><!-- footer_form_left -->
+
+			<div id='footer_form_right'>
+		
+			
+		
+			</div><!-- footer_form_right -->
+		
+		</div><!-- footer_form_inner -->
+	
+	</div><!-- footer_form_wrapper -->
+
+</footer><!-- consultation -->
 		
 		<?php //gravity_form(1, false, false, false, '', true, 12); ?>
 		
