07e331c6ec403ed4ee3dfb7413ca0bca1b767280
Garrett Cullen <Garth619@users.noreply.github.com>
2020-06-04 13:55:28 -0700

    push

---
+++ wp-content/themes/pq10/page-templates/includes/homepage_template_parts/section-7.php
@@ -0,0 +1,23 @@
+<section id='section_seven'>
+
+  <div id='section_seven_inner'>
+
+    <span class='sec_seven_title'>Client Testimonials</span><!-- sec_seven_title -->
+  
+    <div id='sec_seven_slider'>
+
+     <div class='sec_seven_single_slide'>
+      
+        <?php echo file_get_contents( get_template_directory() . '/images/hilley/stars.svg' ) ?>
+
+        <span class='sec_seven_desc'>“Hilley & Frieder restored my health, confidence, faith in legal matters and most of all my trust…</span><!-- sec_seven_desc -->
+
+        <span class='sec_seven_name'>Monique</span><!-- sec_seven_name -->
+      
+      </div><!-- sec_seven_single_slide -->
+    
+    </div><!-- sec_seven_slider -->
+  
+  </div><!-- section_seven_inner -->
+
+</section><!-- section_seven -->
