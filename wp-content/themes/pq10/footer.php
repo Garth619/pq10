7254035c39cd6e5bf0bf1187d8ca1dbc8624c85d
Garrett Cullen <Garth619@users.noreply.github.com>
2020-06-04 15:44:34 -0700

    push

--- wp-content/themes/pq10/footer.php
+++ wp-content/themes/pq10/footer.php
@@ -20,7 +20,7 @@
 
 			<div id='footer_form_right'>
 		
-			
+				<?php gravity_form(1, false, false, false, '', true, 12); ?>
 		
 			</div><!-- footer_form_right -->
 		
@@ -30,7 +30,7 @@
 
 </footer><!-- consultation -->
 		
-		<?php //gravity_form(1, false, false, false, '', true, 12); ?>
+	
 		
 
 					<?php //echo file_get_contents( get_template_directory() . "/images/footer-01.svg" ) ?>
