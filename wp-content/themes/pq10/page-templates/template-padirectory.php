d9f72b40968076143a65d3f3a489213cc1308231
Garrett Cullen <Garth619@users.noreply.github.com>
2020-06-08 18:25:29 -0700

    push

--- wp-content/themes/pq10/page-templates/template-padirectory.php
+++ wp-content/themes/pq10/page-templates/template-padirectory.php
@@ -6,20 +6,16 @@ get_header(); ?>
 
 <div id="internal_main">
 	
-	<div id="internal_banner" class="banner_with_page_title">
-		
-		<div class="internal_banner_content">
+	<div id="internal_banner_two">
 			
-			<h1 class="internal_header"><?php the_title();?></h1><!-- banner_title -->
-			
-		</div><!-- internal_banner_content -->
+		<h1 class='banner_title_two'><?php the_title();?></h1><!-- page_header_two -->
 		
 		<?php $global_internal_banner_image = get_field( 'global_internal_banner_image','option'); ?>
-		<?php $pa_directory_internal_banner = get_field( 'pa_directory_internal_banner' ); ?>
+		<?php $banner_image = get_field( 'banner_image' ); ?>
 		
-		<?php if($pa_directory_internal_banner) : ?>
+		<?php if($banner_image) : ?>
 		
-			<img id="internal_hero" src="<?php echo $pa_directory_internal_banner['url']; ?>" alt="<?php echo $pa_directory_internal_banner['alt']; ?>"/><!-- internal_hero -->
+			<img id="internal_hero" src="<?php echo $banner_image['url']; ?>" alt="<?php echo $banner_image['alt']; ?>"/><!-- internal_hero -->
 		
 		<?php else: ?>
 		
@@ -27,33 +23,38 @@ get_header(); ?>
 		
 		<?php endif;?>
 		
-	</div><!-- internal_banner -->
+	</div><!-- internal_banner_two -->
 	
-	<div class="pa_directory">
+	<div class="page_container">
+		
+		<div id='pa_directory_wrapper' class='box_wrapper'>
 		
 		<?php if(get_field('practice_area_directory')): ?>
 		
-			<ul class="pa_directory_top_menu">
-				
-				<?php while(has_sub_field('practice_area_directory')): ?>
-		 
-					<li>
-						
-						<a><?php the_sub_field( 'title' ); ?></a>
-					
-						<?php $obj = get_sub_field('pa_nav_menu'); ?>
-				
-						<?php wp_nav_menu( array( 'menu' => $obj->name) ); ?>
-					 
-					 </li>
-					
-				<?php endwhile; ?>
+		<ul class="pa_directory_top_menu">
 			
-			</ul><!-- pa_directory_top_menu -->
-		 
-		<?php endif; ?>
+			<?php while(has_sub_field('practice_area_directory')): ?>
+	 
+				<li>
+					
+					<a><?php the_sub_field( 'title' ); ?></a>
+				
+					<?php $obj = get_sub_field('pa_nav_menu'); ?>
+			
+					<?php wp_nav_menu( array( 'menu' => $obj->name) ); ?>
+				 
+				 </li>
+				
+			<?php endwhile; ?>
 		
-	</div><!-- pa_directory -->
+		</ul><!-- pa_directory_top_menu -->
+	 
+	<?php endif; ?>
+		
+		</div><!-- pa_directory_wrapper -->
+		
+	</div><!-- page_container -->
+	
 	
 </div><!-- internal_main -->
 		
