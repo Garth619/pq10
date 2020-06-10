d51891b369e8428617a902017eda0d149f4c697b
Garrett Cullen <Garth619@users.noreply.github.com>
2020-06-09 20:27:47 -0700

    push

--- wp-content/themes/pq10/404.php
+++ wp-content/themes/pq10/404.php
@@ -1,24 +1,47 @@
 <?php get_header(); ?>
 
-<div id="internal_main" class="no_banner">
+<div id="internal_main">
 	
-	<div class="internal_container">
-		
-		<h1 class="internal_header"><?php the_field( 'not_found_title','option'); ?></h1>
-		
-		<div class="not_found inner_container">
-		
-			<span class="error"><?php the_field( 'not_found_subtitle','option'); ?></span><!-- error -->
+	<div id="internal_banner_two">
 			
-			<div class="not_found_content content">
+		<h1 class='banner_title_two'>Not Found</h1><!-- page_header_two -->
 		
-				<?php the_field( 'not_found_content','option'); ?>
-			
-			</div><!-- not_found_content -->
+		<?php $global_internal_banner_image = get_field( 'global_internal_banner_image','option'); ?>
+		<?php $banner_image = get_field( 'banner_image' ); ?>
+		
+		<?php if($banner_image) : ?>
+		
+			<img id="internal_hero" src="<?php echo $banner_image['url']; ?>" alt="<?php echo $banner_image['alt']; ?>"/><!-- internal_hero -->
+		
+		<?php else: ?>
+		
+			<img id="internal_hero" src="<?php echo $global_internal_banner_image['url']; ?>" alt="<?php echo $global_internal_banner_image['alt']; ?>"/><!-- internal_hero -->
+		
+		<?php endif;?>
+		
+	</div><!-- internal_banner_two -->
+	
+	<div class="page_container">
+		
+		<div id='not_found_wrapper' class='box_wrapper'>
+		
+			<span id='error'>Error code: 404</span><!-- error -->
 
-		</div><!-- video_wrapper -->
+			<span id='not_found_content'>The page you were looking for appears to have been moved, deleted or does not exist. You could go back to where you were or head straight to our home page.</span><!-- not_found_content -->
+		
+			<div id='not_found_button_wrapper'>
+			
+				<span class='button_two not_found_button'>Go back</span><!-- button_two -->
+
+				<a class='button_two not_found_button' href="<?php bloginfo('url');?>">Home</a><!-- button_two -->
+			
+			</div><!-- not_found_button_wrapper -->
+
+		</div><!-- not_found_wrapper -->
+		
+	</div><!-- page_container -->
 	
-	</div><!-- internal_main -->
+</div><!-- internal_main -->
 
 
 <?php get_footer(); ?>
