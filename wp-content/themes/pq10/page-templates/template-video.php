108fc6603aab53bf75c2b4a433d6e613efb4285d
Garrett Cullen <Garth619@users.noreply.github.com>
2020-06-09 18:56:42 -0700

    push

--- wp-content/themes/pq10/page-templates/template-video.php
+++ wp-content/themes/pq10/page-templates/template-video.php
@@ -11,12 +11,17 @@ get_header(); ?>
 		<h1 class="page_header center"><?php the_title();?></h1><!-- page_header -->
 		
 		<div id='video_wrapper'>
-		
+
+		<?php if ( have_rows( 'video_center' ) ) : ?>
+			<?php while ( have_rows( 'video_center' ) ) : the_row(); ?>
+
+			<?php if(get_field('wistia_or_youtube') == "Wistia") { ?>
+	
 			<div class='single_video'>
 
 				<div class='video_thumb'>
 				
-					<div class='mywisita wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail'></div><!-- mywisita -->
+					<div class='mywisita wistia_embed wistia_async_<?php the_sub_field( 'wistia_id' ); ?> popover=true popoverContent=thumbnail'></div><!-- mywisita -->
 
 					<div class='video_overlay'>
 					
@@ -28,17 +33,23 @@ get_header(); ?>
 
 				<div class='video_title_wrapper'>
 				
-					<span class='video_title'>Lorem ipsum dolor title here two lines lorem ispsum</span><!-- video_title -->
+					<span class='video_title'><?php the_sub_field( 'video_title' ); ?></span><!-- video_title -->
 				
 				</div><!-- video_title_wrapper -->
 				
 			</div><!-- single_video -->
 
-			<div class='single_video'>
+			<?php } ?>
+
+			<?php if(get_field('wistia_or_youtube') == "Youtube") { ?>
+
+				<div class='single_video'>
+
+				<a href="https://www.youtube.com/embed/<?php the_sub_field( 'youtube_id' ); ?>" data-lity>
 
 				<div class='video_thumb'>
 				
-					<div class='mywisita wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail'></div><!-- mywisita -->
+					<img class="youtube" src="https://img.youtube.com/vi/<?php the_sub_field( 'youtube_id' ); ?>/0.jpg"/>
 
 					<div class='video_overlay'>
 					
@@ -50,55 +61,20 @@ get_header(); ?>
 
 				<div class='video_title_wrapper'>
 				
-					<span class='video_title'>Lorem ipsum dolor title here two lines lorem ispsum</span><!-- video_title -->
+					<span class='video_title'><?php the_sub_field( 'video_title' ); ?></span><!-- video_title -->
 				
 				</div><!-- video_title_wrapper -->
+
+				</a>
 				
 			</div><!-- single_video -->
 
-			<div class='single_video'>
+			<?php } ?>
 
-				<div class='video_thumb'>
-				
-					<div class='mywisita wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail'></div><!-- mywisita -->
+			<?php endwhile; ?>
+		<?php endif; ?>
 
-					<div class='video_overlay'>
-					
-						<span class='play_button'></span><!-- play_button -->
-					
-					</div><!-- video_overlay -->
-				
-				</div><!-- video_thumb -->
-
-				<div class='video_title_wrapper'>
-				
-					<span class='video_title'>Lorem ipsum dolor title here two lines lorem ispsum</span><!-- video_title -->
-				
-				</div><!-- video_title_wrapper -->
-				
-			</div><!-- single_video -->
-
-			<div class='single_video'>
-
-				<div class='video_thumb'>
-				
-					<div class='mywisita wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail'></div><!-- mywisita -->
-
-					<div class='video_overlay'>
-					
-						<span class='play_button'></span><!-- play_button -->
-					
-					</div><!-- video_overlay -->
-				
-				</div><!-- video_thumb -->
-
-				<div class='video_title_wrapper'>
-				
-					<span class='video_title'>Lorem ipsum dolor title here two lines lorem ispsum</span><!-- video_title -->
-				
-				</div><!-- video_title_wrapper -->
-				
-			</div><!-- single_video -->
+			
 		
 		</div><!-- video_wrapper -->
 		
@@ -106,6 +82,8 @@ get_header(); ?>
 	
 </div><!-- internal_main -->
 
-<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+<?php if(get_field('wistia_or_youtube') == "Wistia") { ?>
+	<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+<?php } ?>
 
 <?php get_footer(); ?>
